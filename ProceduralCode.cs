bc20fdb0c7b5c841eb2543819315383f93a04b58
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-04-14 19:28:01 +0000

    WE HOPE

--- ProceduralCode.cs
+++ ProceduralCode.cs
@@ -1,16 +1,25 @@
 ﻿namespace knightmoves;
-public class ProceduralCode
-{
-    public int length = 5;
-    public int height = 10;
-
-    public int CalculateArea(int length, int height){
-        return length * height;
+public class ProceduralCode {
+    public void Run(){
+        Rectangle rectangle = new Rectangle(1, 2);
+        int area = rectangle.CalculateArea();
     }
 }
 
 // Add your code here
+public class Rectangle{ 
+    private int length;
+    private int height;
 
+    public Rectangle (int length, int height){
+        this.length = length;
+        this.height = height;
+    }
+
+    public int CalculateArea(){
+        return length * height;
+    }
+}
 
 //should encapsulate the variables and methods within a Rectangle class
 //should pass the variables into the Rectangle as constructor arguments
